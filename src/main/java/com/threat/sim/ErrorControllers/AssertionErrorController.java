7673ffb39f67105d887996e381d155a985a355ea
Anotheraq <drevniykek@gmail.com>
2022-11-17 15:22:00 +0100

    Created error controllers

---
+++ src/main/java/com/threat/sim/ErrorControllers/AssertionErrorController.java
@@ -0,0 +1,24 @@
+package com.threat.sim.ErrorControllers;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AssertionErrorController {
+
+    @GetMapping(value = "/aerr")
+    public String render(Model model) {
+        model.addAttribute("title","Assertion error");
+        try{
+            Assertions.assertEquals(2,4);
+        }catch (AssertionError e){
+            model.addAttribute("message", e);
+            model.addAttribute("trace", ExceptionUtils.getStackTrace(e));
+        }
+        return "exception-errors";
+    }
+}
---
+++ src/main/java/com/threat/sim/ErrorControllers/StackOverflowErrorController.java
@@ -0,0 +1,26 @@
+package com.threat.sim.ErrorControllers;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class StackOverflowErrorController {
+
+    @GetMapping(value = "/soerr")
+    public String render(Model model) {
+        model.addAttribute("title","Stack overflow error");
+        try{
+            err(Integer.MIN_VALUE);
+        }catch (StackOverflowError e){
+            model.addAttribute("message", e);
+            model.addAttribute("trace", ExceptionUtils.getStackTrace(e));
+        }
+        return "exception-errors";
+    }
+    private void err(int i){
+        err(i+1);
+    }
+}
